4a1f1a7429f1a511b0e2f82dafe32b0cbf8b6a7a
Giorgio-24 <cipuandrei@libero.it>
2022-02-20 19:27:15 +0100

    clinic model created

---
+++ src/main/java/com/example/model/Clinic.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+
+import javax.persistence.*;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "Clinics")
+
+public class Clinic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+
+    private String description;
+}
