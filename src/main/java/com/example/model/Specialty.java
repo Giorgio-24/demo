7055966974544d71a2bd9fb587804e9236aa708a
Giorgio-24 <cipuandrei@libero.it>
2022-02-20 19:23:37 +0100

    specialty model created

---
+++ src/main/java/com/example/model/Specialty.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+
+import javax.persistence.*;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//? Da controllare se può dare fastidio il nome della entità con la Y, come su Laravel.
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "Specialties")
+
+public class Specialty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
