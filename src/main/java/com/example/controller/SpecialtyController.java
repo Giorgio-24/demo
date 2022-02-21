08879c776179d8cf41c08127f0c82019cc3ec8df
Giorgio-24 <cipuandrei@libero.it>
2022-02-21 17:30:52 +0100

    specialty controller done

---
+++ src/main/java/com/example/controller/SpecialtyController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.dto.SpecialtyDTO;
+import com.example.service.SpecialtyService;
+
+@RestController
+@RequestMapping("/specialty")
+@CrossOrigin(origins = "http://localhost:4200")
+public class SpecialtyController extends AbstractController<SpecialtyDTO> {
+
+    @Autowired
+    private SpecialtyService specialtyService;
+
+}
